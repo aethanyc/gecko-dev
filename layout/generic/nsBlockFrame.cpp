043f0a69d0fa449eff77a3168c08ff02ac8e26c1
Ting-Yu Lin <tlin@mozilla.com>
2017-08-29 17:45:38 +0800

    Bug 1340966 - Asserting SetNextInFlowNeedsReflow() should be fully complete when called.
    The comment "you wouldn't set mNextInFlowNeedsReflow IsComplete() is true"
    was written before OverflowIncomplete was introduced in bug 379349.
    Nowadays, nsGridContainerFrame sets this bit with OverflowIncomplete.
    
    So I believe we should assert IsFullyComplete() in
    SetNextInFlowNeedsReflow() rather than assert IsComplete().
    
    Other modifications are setting incompletion status for
    SetNextInFlowNeedsReflow() callers.
    
    [1] http://searchfox.org/mozilla-central/rev/f2a1911ad310bf8651f342d719e4f4ca0a7b9bfb/layout/generic/nsGridContainerFrame.cpp#5549-5550
    
    MozReview-Commit-ID: 2yrL79jOV8A

--- layout/generic/nsBlockFrame.cpp
+++ layout/generic/nsBlockFrame.cpp
@@ -4169,6 +4169,7 @@ nsBlockFrame::DoReflowInlineFrames(BlockReflowInput& aState,
     if (iter.Next() && iter.GetLine()->IsInline()) {
       iter.GetLine()->MarkDirty();
       if (iter.GetContainer() != this) {
+        aState.mReflowStatus.SetIncomplete();
         aState.mReflowStatus.SetNextInFlowNeedsReflow();
       }
     }
@@ -6405,7 +6406,9 @@ nsBlockFrame::ReflowFloat(BlockReflowInput& aState,
   }
 
   if (aReflowStatus.NextInFlowNeedsReflow()) {
-    aState.mReflowStatus.SetNextInFlowNeedsReflow();
+    MOZ_ASSERT(!aReflowStatus.IsFullyComplete(),
+               "aReflowStatus shouldn't be fully complete!");
+    aState.mReflowStatus.MergeCompletionStatusFrom(aReflowStatus);
   }
 
   if (aFloat->IsLetterFrame()) {
