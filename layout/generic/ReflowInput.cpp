e2d38cfa056be5aaaac5530b4384a29314331aff
Ting-Yu Lin <tlin@mozilla.com>
2024-06-10 12:28:25 -0700

    Bug 1901652 Part 3 - Fix hypothetical position for fixed-pos elements when scrollbar-gutters are on both-edges. r?#layout
    We have similar adjustments in `ViewportFrame::AdjustReflowInputForScrollbars()`
    and `nsComputedDOMStyle::GetUsedAbsoluteOffset`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D213345

--- layout/generic/ReflowInput.cpp
+++ layout/generic/ReflowInput.cpp
@@ -13,6 +13,7 @@
 #include "CounterStyleManager.h"
 #include "LayoutLogging.h"
 #include "mozilla/dom/HTMLInputElement.h"
+#include "mozilla/ScrollContainerFrame.h"
 #include "mozilla/WritingModes.h"
 #include "nsBlockFrame.h"
 #include "nsFlexContainerFrame.h"
@@ -1415,8 +1416,17 @@ void ReflowInput::CalculateHypotheticalPosition(
   // The current coordinate space is that of the nearest block to the
   // placeholder. Convert to the coordinate space of the absolute containing
   // block.
-  nsPoint cbOffset =
-      containingBlock->GetOffsetToIgnoringScrolling(aCBReflowInput->mFrame);
+  const nsIFrame* cbFrame = aCBReflowInput->mFrame;
+  nsPoint cbOffset = containingBlock->GetOffsetToIgnoringScrolling(cbFrame);
+  if (cbFrame->IsViewportFrame()) {
+    // When the containing block is the ViewportFrame, i.e. we are calculating
+    // the static position for a fixed positioned frame, we need to adjust the
+    // origin to exclude the scrollbar or scrollbar-gutter area.
+    if (ScrollContainerFrame* sf = cbFrame->GetScrollTargetFrame()) {
+      const nsMargin scrollbarSizes = sf->GetActualScrollbarSizes();
+      cbOffset.MoveBy(-scrollbarSizes.left, -scrollbarSizes.top);
+    }
+  }
 
   nsSize reflowSize = aCBReflowInput->ComputedSizeAsContainerIfConstrained();
   LogicalPoint logCBOffs(wm, cbOffset, reflowSize - containerSize);
