241ba654dd9a665f64592cee0ac2847a98cadc2c
Ting-Yu Lin <tlin@mozilla.com>
2024-07-29 13:45:31 -0700

    Bug 1909761 Part 2 - Create a helper struct IntrinsicSizeInput to aggregate needed data when computing intrinsic inline size. r?dholbert,#layout
    This patch changes the signature to `GetMinISize()`, `GetPrefISize()`,
    `IntrinsicISize` by adding a helper struct as a preparation. Then we can just
    add more data such as a percentage basis to the struct without altering the
    signature in the future.
    
    When passing `IntrinsicSizeInput` struct down to another helper method, we
    generally just pass the original one if the method is computing the intrinsic
    size of our own or our anonymous children. If the method is computing our
    children's intrinsic contribution, we'll need to create a brand new
    `IntrinsicSizeInput` for our children.

--- layout/base/nsLayoutUtils.cpp
+++ layout/base/nsLayoutUtils.cpp
@@ -4396,10 +4396,12 @@ static Maybe<nscoord> GetIntrinsicSize(nsIFrame::ExtremumLength aLength,
   nscoord result;
   if (aISizeFromAspectRatio) {
     result = *aISizeFromAspectRatio;
-  } else if (aLength == nsIFrame::ExtremumLength::MaxContent) {
-    result = aFrame->GetPrefISize(aRenderingContext);
   } else {
-    result = aFrame->GetMinISize(aRenderingContext);
+    const IntrinsicSizeInput input{aRenderingContext};
+    auto type = aLength == nsIFrame::ExtremumLength::MaxContent
+                    ? IntrinsicISizeType::PrefISize
+                    : IntrinsicISizeType::MinISize;
+    result = aFrame->IntrinsicISize(input, type);
   }
 
   result += aContentBoxToBoxSizingDiff;
@@ -4525,8 +4527,9 @@ static nscoord AddIntrinsicSizeOffset(
     if (aISizeFromAspectRatio) {
       minContent = maxContent = *aISizeFromAspectRatio;
     } else {
-      minContent = aFrame->GetMinISize(aRenderingContext);
-      maxContent = aFrame->GetPrefISize(aRenderingContext);
+      const IntrinsicSizeInput input{aRenderingContext};
+      minContent = aFrame->GetMinISize(input);
+      maxContent = aFrame->GetPrefISize(input);
     }
     minContent += contentBoxToBoxSizingDiff;
     maxContent += contentBoxToBoxSizingDiff;
@@ -4819,7 +4822,8 @@ nscoord nsLayoutUtils::IntrinsicForAxis(
         result = aFrame->BSize();
       }
     } else {
-      result = aFrame->IntrinsicISize(aRenderingContext, aType);
+      const IntrinsicSizeInput input{aRenderingContext};
+      result = aFrame->IntrinsicISize(input, aType);
     }
 #ifdef DEBUG_INTRINSIC_WIDTH
     --gNoiseIndent;
@@ -4951,9 +4955,10 @@ nscoord nsLayoutUtils::IntrinsicForAxis(
   }
 
   if (aFrame->IsTableFrame()) {
-    // Tables can't shrink smaller than their intrinsic minimum width,
+    // Tables can't shrink smaller than their intrinsic minimum inline size,
     // no matter what.
-    min = aFrame->GetMinISize(aRenderingContext);
+    const IntrinsicSizeInput input{aRenderingContext};
+    min = aFrame->GetMinISize(input);
   }
 
   // If we have an aspect-ratio and a definite block size of |aFrame|, we should
