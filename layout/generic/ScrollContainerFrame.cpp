d9ed61e17416f9f690e54890a412d5601f89b935
Ting-Yu Lin <tlin@mozilla.com>
2024-07-29 13:45:31 -0700

    Create a helper struct IntrinsicSizeInput to aggregate needed data when computing intrinsic inline size.
    This patch changes the signature to `GetMinISize()`, `GetPrefISize()`,
    `IntrinsicISize` by adding a helper struct as a preparation. Then we can just
    add more data such as a percentage basis to the struct without altering the
    signature in the future.
    
    When passing `IntrinsicSizeInput` struct down to another helper method, we
    generally just pass the original one if the method is computing the intrinsic
    size of our own or our anonymous children. If the method is computing our
    children's intrinsic contribution, we'll need to create a brand new
    `IntrinsicSizeInput` for our children.

--- layout/generic/ScrollContainerFrame.cpp
+++ layout/generic/ScrollContainerFrame.cpp
@@ -1271,7 +1271,7 @@ static bool IsMarqueeScrollbox(const nsIFrame& aScrollFrame) {
   return HTMLMarqueeElement::FromNodeOrNull(aScrollFrame.GetContent());
 }
 
-nscoord ScrollContainerFrame::IntrinsicISize(gfxContext* aContext,
+nscoord ScrollContainerFrame::IntrinsicISize(const IntrinsicSizeInput& aInput,
                                              IntrinsicISizeType aType) {
   nscoord result = [&] {
     if (const Maybe<nscoord> containISize = ContainIntrinsicISize()) {
@@ -1281,7 +1281,7 @@ nscoord ScrollContainerFrame::IntrinsicISize(gfxContext* aContext,
         MOZ_UNLIKELY(IsMarqueeScrollbox(*this))) {
       return 0;
     }
-    return mScrolledFrame->IntrinsicISize(aContext, aType);
+    return mScrolledFrame->IntrinsicISize(aInput, aType);
   }();
 
   return NSCoordSaturatingAdd(result,
